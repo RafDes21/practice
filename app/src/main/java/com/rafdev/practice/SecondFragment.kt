cf657a80f8f939cace8a0e0d3c9fbdc881f28e79
RafDes21 <israeltineo84@gmail.com>
2024-01-11 19:42:00 -0300

    practice form

--- app/src/main/java/com/rafdev/practice/SecondFragment.kt
+++ app/src/main/java/com/rafdev/practice/SecondFragment.kt
@@ -25,31 +25,33 @@ class SecondFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        // Recuperar el estado actual desde SharedPreferences
-        val selectedTab = loadSelectedTab()
-
-        if (savedInstanceState == null) {
-            // Solo realizar la transacción inicial si no hay un estado guardado (evitar duplicados)
             childFragmentManager.beginTransaction()
-                .replace(
-                    R.id.profileContainerFragment,
-                    when (selectedTab) {
-                        4 -> FourthFragment()
-                        5 -> FiveFragment()
-                        6 -> SixFragment()
-                        else -> FourthFragment()  // Fragment por defecto en caso de que no se haya guardado ningún estado
-                    }
-                )
+                .replace(R.id.profileContainerFragment, RegisterFragment())
                 .commit()
-        }
+        // Recuperar el estado actual desde SharedPreferences
+//        val selectedTab = loadSelectedTab()
+
+//        if (savedInstanceState == null) {
+//            // Solo realizar la transacción inicial si no hay un estado guardado (evitar duplicados)
+//            childFragmentManager.beginTransaction()
+//                .replace(
+//                    R.id.profileContainerFragment,
+//                    when (selectedTab) {
+//                        4 -> FourthFragment()
+//                        5 -> FiveFragment()
+//                        6 -> SixFragment()
+//                        else -> FourthFragment()  // Fragment por defecto en caso de que no se haya guardado ningún estado
+//                    }
+//                )
+//                .commit()
+//        }
     }
 
-    private fun loadSelectedTab(): Int {
-        val sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
-        return sharedPreferences.getInt(
-            SELECTED_TAB_KEY,
-            4
-        ) // Valor por defecto es 4 si no hay nada guardado
-    }
+//    private fun loadSelectedTab(): Int {
+//        val sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
+//        return sharedPreferences.getInt(
+//            SELECTED_TAB_KEY,
+//            4
+//        ) // Valor por defecto es 4 si no hay nada guardado
+//    }
 }
