b0197448db6881eeac8fd8b8d5480143a93a3a43
RafDes21 <israeltineo84@gmail.com>
2024-01-14 16:55:45 -0300

    formulario complete

--- app/src/main/java/com/rafdev/practice/ui/signin/SignInActivity.kt
+++ app/src/main/java/com/rafdev/practice/ui/signin/SignInActivity.kt
@@ -5,9 +5,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import com.rafdev.practice.R
+import com.rafdev.practice.core.onTextChanged
 import com.rafdev.practice.databinding.ActivitySignInBinding
 import com.rafdev.practice.ui.fragment.third.ThirdFragment
 import com.rafdev.practice.ui.login.LoginActivity
+import com.rafdev.practice.ui.signin.model.UserSignIn
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,7 +25,7 @@ class SignInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInBinding
 
-    private val viewModel:SignInViewModel by viewModels()
+    private val viewModel: SignInViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
@@ -32,20 +37,80 @@ class SignInActivity : AppCompatActivity() {
         initListeners()
         initObservers()
     }
+
     private fun initListeners() {
-        binding.viewBottom.tvFooter.setOnClickListener {viewModel.onLoginSelected() }
-        binding.viewHeader.ivLogo.setOnClickListener{finish()}
+        binding.etRepeatPassword.onTextChanged { onFieldChanged() }
+        binding.etPassword.onTextChanged { onFieldChanged() }
+        binding.etRealName.onTextChanged { onFieldChanged() }
+        binding.etNickname.onTextChanged { onFieldChanged() }
+        binding.etEmail.onTextChanged { onFieldChanged() }
+
+        binding.viewBottom.tvFooter.setOnClickListener { viewModel.onLoginSelected() }
+        binding.viewHeader.ivLogo.setOnClickListener { finish() }
+
+        with(binding) {
+            btnCreateAccount.setOnClickListener {
+                viewModel.onSignInSelected(
+                    UserSignIn(
+                        realName = binding.etRealName.text.toString(),
+                        nickName = binding.etNickname.text.toString(),
+                        email = binding.etEmail.text.toString(),
+                        password = binding.etPassword.text.toString(),
+                        passwordConfirmation = binding.etRepeatPassword.text.toString()
+                    )
+                )
+            }
+        }
+
     }
+
     private fun initObservers() {
         viewModel.navigateToLogin.observe(this) {
             it.getContentIfNotHandled()?.let {
                 goToLogin()
             }
         }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.viewState.collect { viewState ->
+                updateUI(viewState)
+            }
+        }
+
     }
 
     private fun goToLogin() {
         startActivity(LoginActivity.create(this))
         finish()
     }
+
+    private fun updateUI(viewState: SignInViewState) {
+        with(binding) {
+            pbLoading.isVisible = viewState.isLoading
+            binding.tilEmail.error =
+                if (viewState.isValidEmail) null else getString(R.string.signin_error_mail)
+            binding.tilNickname.error =
+                if (viewState.isValidNickName) null else getString(R.string.signin_error_nickname)
+            binding.tilRealName.error =
+                if (viewState.isValidRealName) null else getString(R.string.signin_error_realname)
+            binding.tilPassword.error =
+                if (viewState.isValidPassword) null else getString(R.string.signin_error_password)
+            binding.tilRepeatPassword.error =
+                if (viewState.isValidPassword) null else getString(R.string.signin_error_password)
+        }
+    }
+    private fun onFieldChanged(hasFocus: Boolean = false) {
+        if (!hasFocus) {
+            viewModel.onFieldsChanged(
+                UserSignIn(
+                    realName = binding.etRealName.text.toString(),
+                    nickName = binding.etNickname.text.toString(),
+                    email = binding.etEmail.text.toString(),
+                    password = binding.etPassword.text.toString(),
+                    passwordConfirmation = binding.etRepeatPassword.text.toString()
+                )
+            )
+        }
+    }
+
 }
--- app/src/main/java/com/rafdev/practice/ui/signin/SignInViewModel.kt
+++ app/src/main/java/com/rafdev/practice/ui/signin/SignInViewModel.kt
@@ -1,22 +1,69 @@
 package com.rafdev.practice.ui.signin
 
+import android.util.Log
+import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.rafdev.practice.core.Event
+import com.rafdev.practice.ui.signin.model.UserSignIn
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class SignInViewModel @Inject constructor() :ViewModel() {
+
+    private companion object {
+        const val MIN_PASSWORD_LENGTH = 6
+    }
 
 
     private val _navigateToLogin = MutableLiveData<Event<Boolean>>()
     val navigateToLogin: LiveData<Event<Boolean>>
         get() = _navigateToLogin
 
+    private val _viewState = MutableStateFlow(SignInViewState())
+    val viewState: StateFlow<SignInViewState>
+        get() = _viewState
     fun onLoginSelected() {
         _navigateToLogin.value = Event(true)
+    }
+
+    fun onSignInSelected(userSignIn: UserSignIn) {
+
+        Log.i("user", "$userSignIn")
+        val viewState = userSignIn.toSignInViewState()
+        if (viewState.userValidated() && userSignIn.isNotEmpty()) {
+            Log.i("user", "salio todo bien $userSignIn")
+//            signInUser(userSignIn)
+        } else {
+            Log.i("user", "salio todo mal $userSignIn")
+            onFieldsChanged(userSignIn)
+        }
+    }
+
+    fun onFieldsChanged(userSignIn: UserSignIn) {
+        _viewState.value = userSignIn.toSignInViewState()
+    }
+
+    private fun isValidOrEmptyEmail(email: String) =
+        Patterns.EMAIL_ADDRESS.matcher(email).matches() || email.isEmpty()
+
+    private fun isValidOrEmptyPassword(password: String, passwordConfirmation: String): Boolean =
+        (password.length >= MIN_PASSWORD_LENGTH && password == passwordConfirmation) || password.isEmpty() || passwordConfirmation.isEmpty()
+
+    private fun isValidName(name: String): Boolean =
+        name.length >= MIN_PASSWORD_LENGTH || name.isEmpty()
+
+    private fun UserSignIn.toSignInViewState(): SignInViewState {
+        return SignInViewState(
+            isValidEmail = isValidOrEmptyEmail(email),
+            isValidPassword = isValidOrEmptyPassword(password, passwordConfirmation),
+            isValidNickName = isValidName(nickName),
+            isValidRealName = isValidName(realName)
+        )
     }
 
 
